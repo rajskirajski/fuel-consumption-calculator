1bd9994230cbdb746c79803d1c7cf1228aab022e
Przemyslaw <przemekrajski@gmail.com>
2026-07-13 19:47:10 +0200

    Restore calculator previous version

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Fuel Consumption Calculator"
-    version: str = "1.0.1"
+    version: str = "1.0.0"
     environment: str = "development"
 
     model_config = SettingsConfigDict(
